57ec005a1be04be66d21a718f3ecbf51d0a75850
Matias Almeida <matiasleonelalmeida97@gmail.com>
2023-10-16 14:05:20 -0300

    configured app routing

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'sign-up', component: RegisterPageComponent },
+      { path: '**', redirectTo: 'sign-up' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
